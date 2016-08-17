172107f1849671ea7f610998c4a2e532127a9954
AshokChava <ashok.chava@gmail.com>
2016-08-17 10:49:52 +0530

    Revert "init"
    This reverts commit fbff3cfa569fab253ad1932a773ea56e7a9e2623.

--- webproject/popup/webproject.js
+++
@@ -1,6 +0,0 @@
-chrome.tabs.executeScript(null, {
-    file: "/content_scripts/main.js"
-  });
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {queryString: "You are good"});
-  });
